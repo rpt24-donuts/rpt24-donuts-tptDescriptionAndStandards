5e04f8c3f84cf01dad305934d72a59c888dd3bec
Jake <jvanboch@gmail.com>
2020-11-30 13:53:50 -0800

    updated variable and table names from app plan feedback. Updated testing to be more robust when different database data is added in

--- index.js
+++ index.js
@@ -21,14 +21,19 @@ app.get('/:Id/DS', (req, res) => {
   const productInfo = {};
   con.query(`select * from Product where id = ${productId};`, (productQueryErr, productQueryResult) => {
     if (productQueryErr) throw productQueryErr;
-
-    productInfo.productDescriptions = productQueryResult[0].Descriptions;
-    productInfo.pageLength = productQueryResult[0].Pages;
-    productInfo.answerKeyIncluded = productQueryResult[0].answer_key;
-    productInfo.teachingDuration = productQueryResult[0].teaching_dur;
+    if (productQueryResult[0].answerKeyIncluded === 1) {
+      productInfo.answerKeyIncluded = 'Yes'
+     }else if(productQueryResult[0].answerKeyIncluded === 0) {
+      productInfo.answerKeyIncluded = 'No'
+    } else {
+      productInfo.answerKeyIncluded = 'N/A'
+    }
+    productInfo.productDescriptions = productQueryResult[0].productDescription;
+    productInfo.pageLength = productQueryResult[0].pageLength;
+    productInfo.teachingDuration = productQueryResult[0].teachingDuration;
     productInfo.standards = [];
     productInfo.standardsDescription = [];
-    con.query(`select * from SandD where Product_id = ${productId};`, (querySandDErr, resultSandD) => {
+    con.query(`select * from StandardsandDescriptions where Product_id = ${productId};`, (querySandDErr, resultSandD) => {
       if (querySandDErr) throw querySandDErr;
       const standardIds = [];
       if (resultSandD.length !== 0) {
@@ -37,8 +42,8 @@ app.get('/:Id/DS', (req, res) => {
           con.query(`select * from Standards where ID = ${id}`, (standardQueryError, standardQueryResult) => {
             if (standardQueryError) throw standardQueryError;
 
-            productInfo.standards.push(standardQueryResult[0].Standards);
-            productInfo.standardsDescription.push(standardQueryResult[0].Description);
+            productInfo.standards.push(standardQueryResult[0].standards);
+            productInfo.standardsDescription.push(standardQueryResult[0].standardsDescription);
             if (i === standardIds.length - 1) {
               res.json(productInfo);
             }
--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE SandD;
 USE SandD;
 SET GLOBAL FOREIGN_KEY_CHECKS=0;
 
-CREATE TABLE Product (ID int NOT NULL AUTO_INCREMENT, Descriptions TEXT NOT NULL, Pages int,answer_key varchar(50), teaching_dur varchar(50), PRIMARY KEY (ID));
-CREATE TABLE Standards (ID int NOT NULL AUTO_INCREMENT, Standards varchar(255) NOT NULL, Description TEXT, PRIMARY KEY (ID));
-CREATE TABLE SandD (ID int NOT NULL AUTO_INCREMENT,Product_id int, FOREIGN KEY (Product_id) REFERENCES Product(ID),Standards_id int, FOREIGN KEY (Standards_id) REFERENCES Standards(ID), PRIMARY KEY (ID));
+CREATE TABLE Product (ID int NOT NULL AUTO_INCREMENT, productDescription TEXT NOT NULL, pageLength int,answerKeyIncluded BOOLEAN, teachingDuration varchar(50), PRIMARY KEY (ID));
+CREATE TABLE Standards (ID int NOT NULL AUTO_INCREMENT, standards varchar(255) NOT NULL, standardsDescription TEXT, PRIMARY KEY (ID));
+CREATE TABLE  (ID int NOT NULL AUTO_INCREMENT,Product_id int, FOREIGN KEY (Product_id) REFERENCES Product(ID),Standards_id int, FOREIGN KEY (Standards_id) REFERENCES Standards(ID), PRIMARY KEY (ID));
 --  mysql -u root -p  < schema.sql
