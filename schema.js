2f5c97b06a5514249eba7652cdff58c2a905b8c9
Jake <jvanboch@gmail.com>
2020-12-04 13:56:49 -0800

    removed unneeded console.log

--- schema.js
+++ schema.js
@@ -33,8 +33,6 @@ for (let i = 0; i < 101; i += 1) {
   const randomStandard = Math.floor(Math.random() * standards.length);
 
   for (let j = 0; j < rand; j += 1) {
-
-
     con.query(`insert into StandardsandDescriptions (Product_id, Standards_id) values (${i}, ${randomStandard + j});`, (err, result) => {
       if (err) throw err;
       console.log(result);
