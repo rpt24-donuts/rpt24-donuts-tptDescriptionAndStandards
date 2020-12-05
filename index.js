a0159d5e3f8adcc3b495e2be7dedfacc67b7f0b4
Jake <jvanboch@gmail.com>
2020-12-04 17:54:17 -0800

    added in cors

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 const mysql = require('mysql');
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.static(`${__dirname}/client/dist`));
 
 const con = mysql.createConnection({
