734f99084c6e1975e6528d79b8ced3ae06372d81
Kendall Butt <kendallbutt1@gmail.com>
2021-01-25 21:52:56 -0800

    remove comments

--- db/writeStoD.js
+++ db/writeStoD.js
@@ -4,27 +4,6 @@ const generator = require('./dataGenerator.js');
 
 const standards = generator.standardGenerator();
 
-const writer = csvWriter();
-
-// const standardsToDescriptionsGen = () => {
-//   let done = false;
-//   writer.pipe(fs.createWriteStream('standardsToDescriptionsData.csv'));
-//   for (let i = 0; i < 100; i += 1) {
-//     const rand = Math.floor(Math.random() * 4);
-//     const randomStandard = Math.floor(Math.random() * standards.length);
-
-//     for (let j = 0; j < rand; j += 1) {
-//       writer.write({
-//         Product_id: i,
-//         Standards_id: randomStandard + j,
-//       });
-//     }
-//   }
-//   writer.end();
-//   done = true;
-//   return done;
-// };
-
 const writeUsers = fs.createWriteStream('joins.csv');
 writeUsers.write('Product_id,Standards_id\n', 'utf8');
 
@@ -44,14 +23,10 @@ function writeTenMillionUsers(writer, encoding, callback) {
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {
-        // see if we should continue, or wait
-        // don't pass the callback, because we're not done yet.
         ok = writer.write(data, encoding);
       }
     } while (i > 0 && ok);
     if (i > 0) {
-      // had to stop early!
-      // write some more once it drains
       writer.once('drain', write);
     }
   }
